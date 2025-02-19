aca5635d412b30bdf159dc5d16a6cb13efc1d7c4
hasanhuseyindurak43 <barronsoftwares92@gmail.com>
2025-02-20 01:04:55 +0300

    hasan hüseyin durek

--- myflaskapp/main.py
+++ myflaskapp/main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def get_db_connection():
     conn = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password=" ",
+        user="barron4335",
+        password="1968Hram",
         database="mackolik"
     )
     return conn
@@ -17,6 +17,14 @@ def get_db_connection():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/index_two')
+def index_two():
+    return render_template('index2.html')
+
+@app.route('/index_three')
+def index_three():
+    return render_template('index3.html')
 
 # Tarih formatını düzenleme fonksiyonu
 def format_date(day, month, year):
@@ -28,25 +36,64 @@ def format_date(day, month, year):
 @app.route('/get_matches', methods=['POST'])
 def get_matches():
     data = request.get_json()
+    if not data:
+        return jsonify({'error': 'Geçersiz JSON verisi'}), 400
+
     conn = get_db_connection()
+    if not conn:
+        return jsonify({'error': 'Veritabanı bağlantı hatası'}), 500
+
     cursor = conn.cursor(dictionary=True)
 
-    if 'day' in data and 'month' in data and 'year' in data:
-        day = data['day']
-        month = data['month']
-        year = data['year']
-        date = format_date(day, month, year)
-        cursor.execute("SELECT * FROM maclar WHERE tarih = %s", (date,))
-    elif 'ev' in data and 'misafir' in data:
-        ev = data['ev']
-        misafir = data['misafir']
-        cursor.execute("SELECT * FROM maclar WHERE ev = %s AND misafir = %s", (ev, misafir))
-    else:
-        return jsonify({'error': 'Geçersiz istek'}), 400
+    try:
+        if 'day' in data and 'month' in data and 'year' in data:
+            day = data['day']
+            month = data['month']
+            year = data['year']
+            date = format_date(day, month, year)
+            cursor.execute("SELECT * FROM maclar WHERE tarih = %s", (date,))
+        elif 'ev' in data and 'misafir' in data:
+            ev = data['ev']
+            misafir = data['misafir']
+            cursor.execute("SELECT * FROM maclar WHERE ev = %s AND misafir = %s", (ev, misafir))
+        elif 'iy1' in data and 'iy2' in data and 'ms1' in data and 'ms2' in data:
+            iy1 = data['iy1']
+            iy2 = data['iy2']
+            ms1 = data['ms1']
+            ms2 = data['ms2']
+            iy_skor = f"{iy1} - {iy2}"
+            ms_skor = f"{ms1} - {ms2}"
+            cursor.execute("SELECT * FROM maclar WHERE iy = %s AND ms = %s", (iy_skor, ms_skor))
+        elif 'oddsType' in data and 'oddsValue' in data:
+            odds_type = data['oddsType']
+            odds_value = data['oddsValue']
 
-    matches = cursor.fetchall()
-    conn.close()
-    return jsonify(matches)
+            # Eğer oddsValue bir string ise virgülü noktaya çevir
+            if isinstance(odds_value, str):
+                odds_value = odds_value.replace(',', '.')
+            # Eğer oddsValue bir float ise doğrudan kullan
+            elif isinstance(odds_value, float):
+                pass
+            else:
+                return jsonify({'error': 'Geçersiz oddsValue değeri'}), 400
+
+            # Debugging için sorgu ve parametreleri yazdır
+            query = f"SELECT * FROM maclar WHERE REPLACE(TRIM({odds_type}), ',', '.') = %s"
+            odds_value = str(odds_value).replace(',', '.')
+            print(f"SQL Sorgusu: {query}")
+            print(f"Parametreler: {odds_value}")
+
+            cursor.execute(query, (odds_value,))
+        else:
+            return jsonify({'error': 'Geçersiz istek'}), 400
+
+        matches = cursor.fetchall()
+        return jsonify(matches)
+    except mysql.connector.Error as err:
+        print(f"SQL sorgusu hatası: {err}")
+        return jsonify({'error': 'SQL sorgusu hatası'}), 500
+    finally:
+        conn.close()
 
 # Maç detaylarını getirme endpoint'i
 @app.route('/get_match_details', methods=['POST'])
@@ -62,7 +109,8 @@ def get_match_details():
     conn.close()
 
     if match:
-        details = {key: match[key] for key in list(match.keys())[6:26]}
+        # Detayları JSON olarak döndür
+        details = {key: match[key] for key in list(match.keys())[6:26]}  # İlgili sütunları seç
         return jsonify({'details': details})
     else:
         return jsonify({'error': 'Maç bulunamadı'}), 404
